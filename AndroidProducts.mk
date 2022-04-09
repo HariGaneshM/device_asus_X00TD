c69c8c28cb664ff2a7faabeff512a40de0199fb5
Hari Ganesh <HariGanesh@myinstance.us-west4-b.c.just-armor-341213.internal>
2022-04-09 11:53:45 +0000

    Lets do it for DotOs
    Signed-off-by: Hari Ganesh <HariGanesh@myinstance.us-west4-b.c.just-armor-341213.internal>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_X00TD.mk
+    $(LOCAL_DIR)/dot_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_X00TD-userdebug
+    dot_X00TD-userdebug
 
